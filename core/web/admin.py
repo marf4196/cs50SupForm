a25b4ec8183ce11d0ca35c0fd27a3dee28f7c36f
ramed rafie <marf4196@outlook.com>
2022-07-21 21:42:05 +0430

    admin page fixed for messages model

--- core/web/admin.py
+++ core/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Messages
 
 # Register your models here.
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ['student_name', 'student_lName', 'student_email', 'student_phone', 'subject', 'support_name', 'created_time', 'updated_time']
+    search_fields = ['student_email', 'student_phone', 'support_name']
+    date_hierarchy = 'created_time'
+    ordering = ['-created_time']
+admin.site.register(Messages, MessagesAdmin)
