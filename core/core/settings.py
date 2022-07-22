923ba0c7068a709b0cf0d5ad6bc59d9ca487af7c
ramed rafie <marf4196@outlook.com>
2022-07-22 19:20:59 +0430

    frontend fixed

--- core/core/settings.py
+++ core/core/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_URL =''
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR / 'core' ))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
