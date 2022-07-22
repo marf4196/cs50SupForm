3ac46d0ca410436a29cc6e8796d720246131e91b
ramed rafie <marf4196@outlook.com>
2022-07-22 22:04:00 +0430

    class attend is now working but need some fixes when trying to register after getting error

--- core/web/urls.py
+++ core/web/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from web.views import WelcomePage, FeedBackPage, ClassAttend
+from web.views import WelcomePage, FeedBackPage, Classattend
 
 app_name = 'web'
 
 urlpatterns = [
     path('', WelcomePage.as_view(), name='welcome'),
     path('feedBack', FeedBackPage.as_view(), name='feedBack'),
-    path('class-attend', ClassAttend.as_view(), name='classAttend'),
+    path('class-attend', Classattend.as_view(), name='classattend'),
 ]
--- core/web/views.py
+++ core/web/views.py
@@ -1,6 +1,8 @@
+from email import message
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from .models import ClassAttend
 
 # Create your views here.
 
@@ -13,8 +15,37 @@ class WelcomePage(TemplateView):
 class FeedBackPage(TemplateView):
     template_name = 'feed_back.html'
 
-class ClassAttend(View):
+class Classattend(View):
     def get(self, request, *args, **kwrags):
-        return render(request, 'class_attend_form.html')
+        return render(request, 'class-attend.html')
     def post(self, request, *args, **kwrags):
-        pass
+        name = request.POST['name']
+        ticket = request.POST['ticket']
+        phone = request.POST['phone']
+        email = request.POST['email']
+
+        qs = ClassAttend.objects.filter(ticket = ticket)
+        if not qs.exists():
+            ClassAttend.objects.create(name = name, email = email, phone = phone, ticket = ticket)
+            qs = ClassAttend.objects.get(ticket = ticket)
+            # TODO send sms
+            context = {}
+            context['message'] = 'ثبت نام شما موفق بود'
+            context['qrcode'] = qs.qr_code
+            return render(request, 'class-attend.html', context)
+            
+        else:
+            qs = ClassAttend.objects.get(ticket = ticket)
+            if qs.canceled:
+                qs.canceled = False
+                qs.save()
+                # TODO send sms
+                context = {}
+                context['message'] = 'ثبت نام شما موفق بود'
+                context['qrcode'] = qs.qr_code
+                return render(request, 'class-attend.html', context)
+            else:
+                context = {}
+                context['message'] = 'شما ثبت نام کرده بودید :)'
+                return render(request, 'class-attend.html', context)
+
