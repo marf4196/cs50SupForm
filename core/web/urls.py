4a20c95292206d4856bc2ebfa93aed39f6c39c9a
arminadm <armindarabimahboub@gmail.com>
2022-07-24 02:08:39 +0430

    feedbackView recreated and works fine

--- core/web/urls.py
+++ core/web/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from web.views import WelcomePage, Feedback, FindTicket, ClassAttend, NoSupport, test_template
+from web.views import WelcomePage, FeedbackView, FindTicketView, ClassAttendView, NoSupportView, test_template
 
 app_name = 'web'
 
 urlpatterns = [
     path('test/', test_template.as_view(), name='test_template'),
     path('', WelcomePage.as_view(), name='welcome'),
-    path('feedback/', Feedback.as_view(), name='feedback'),
-    path('class-attend/', ClassAttend.as_view(), name='class-attend'),
-    path('find-ticket/', FindTicket.as_view(), name='find-ticket'),
-    path('no-support/', NoSupport.as_view(), name='no-support')
+    path('feedback/', FeedbackView.as_view(), name='feedback'),
+    path('class-attend/', ClassAttendView.as_view(), name='class-attend'),
+    path('find-ticket/', FindTicketView.as_view(), name='find-ticket'),
+    path('no-support/', NoSupportView.as_view(), name='no-support')
 ]
