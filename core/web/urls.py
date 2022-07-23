8abc84da920d42d3d73ab388a59658ad2b3127ad
ramed rafie <marf4196@outlook.com>
2022-07-23 17:55:03 +0430

    its for armin

--- core/web/urls.py
+++ core/web/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from web.views import WelcomePage, FeedBackPage, Classattend
+from web.views import WelcomePage, FeedBack, Classattend, FindTicket, Nosupport
 
 app_name = 'web'
 
 urlpatterns = [
     path('', WelcomePage.as_view(), name='welcome'),
-    path('feedBack', FeedBackPage.as_view(), name='feedBack'),
+    path('feedBack', FeedBack.as_view(), name='feedBack'),
     path('class-attend', Classattend.as_view(), name='classattend'),
+    path('find-ticket', FindTicket.as_view(), name='findTicket'),
+    path('no-support', Nosupport.as_view(), name='nosupport')
 ]
--- core/web/views.py
+++ core/web/views.py
@@ -1,8 +1,9 @@
 from email import message
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
-from .models import ClassAttend
+from .models import ClassAttend, Feedback, NoSupport, ClassCancel, ClassInfo
 
 # Create your views here.
 
@@ -11,9 +12,6 @@ def indexView(request):
 
 class WelcomePage(TemplateView):
     template_name = 'welcome.html'
-
-class FeedBackPage(TemplateView):
-    template_name = 'feed_back.html'
 
 class Classattend(View):
     def get(self, request, *args, **kwrags):
@@ -32,7 +30,8 @@ class Classattend(View):
             context = {}
             context['message'] = 'ثبت نام شما موفق بود'
             context['qrcode'] = qs.qr_code
-            return render(request, 'class-attend.html', context)
+            context['ticket'] = qs.ticket
+            return render(request, 'find-ticket.html', context)
             
         else:
             qs = ClassAttend.objects.get(ticket = ticket)
@@ -43,9 +42,69 @@ class Classattend(View):
                 context = {}
                 context['message'] = 'ثبت نام شما موفق بود'
                 context['qrcode'] = qs.qr_code
-                return render(request, 'class-attend.html', context)
+                context['ticket'] = qs.ticket
+                return render(request, 'find-ticket.html', context)
             else:
                 context = {}
                 context['message'] = 'شما ثبت نام کرده بودید :)'
                 return render(request, 'class-attend.html', context)
+
+class FindTicket(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'find-ticket.html')
+    def post(self, request, *args, **kwargs):
+        ticket = request.POST['ticket']
+        qs = ClassAttend.objects.filter(ticket = ticket)
+        if qs.exists():
+            qs = ClassAttend.objects.get(ticket = ticket)
+            if qs.canceled:
+                print('shoma sabt nametoon ro ghablan cancel kardid')
+            else:
+                context = {}
+                context['qrcode'] = qs.qr_code
+                return render(request, 'download-page.html', context)
+
+        else:
+            print('sabt nam nakardi')
+
+class FeedBack(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'feed-back.html')
+    def post(self, request, *args, **kwargs):
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        description = request.POST['description']
+
+        Feedback.objects.create(name = name, phone = phone, email = email, subject = subject, description = description)
+
+        context = {}
+        context['message'] = 'ممنون بابت انتقاد/پیشنهادتون \n اطلاعات شما دریافت شد'
+        return render(request, 'feed-back.html', context)
+
+class Nosupport(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'no-support.html')
+    def post(self, request, *args, **kwargs):
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        ticket = request.POST['ticket']
+
+        NoSupport.objects.create(name = name, phone = phone, email = email, ticket = ticket)
+
+        context = {}
+        context['message'] = 'اصطلاعات شما ثبت شد و تا ۲۴ ساعت با شما تماس گرفته خواهد شد'
+        
+        return render(request, 'no-support.html', context)
+
+# class ClassCancel(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'class-cancel.html')
+#     def post(self, request, *args, **kwargs):
+#         phone = request.POST['phone']
+#         email = request.POST['email']
+#         ticket = request.POST['ticket']
+
 
